fc7a487cb162057e306a3b5d0f249787bdd49d68
aditi <ank55@cornell.edu>
2020-01-16 20:21:02 +0530

    added a new page

--- SuryaRating/app/src/main/java/com/example/suryarating/HowLongPage.kt
+++ SuryaRating/app/src/main/java/com/example/suryarating/HowLongPage.kt
@@ -1,9 +1,11 @@
 package com.example.suryarating
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_how_long_page.*
+import kotlinx.android.synthetic.main.content_how_long_page.*
 
 class HowLongPage : AppCompatActivity() {
 
@@ -14,6 +16,9 @@ class HowLongPage : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+        }
+        button_how_long_next.setOnClickListener { _ ->
+            startActivity(Intent(this, VisitsPage::class.java))
         }
     }
 }
--- SuryaRating/app/src/main/java/com/example/suryarating/VisitsPage.kt
+++ SuryaRating/app/src/main/java/com/example/suryarating/VisitsPage.kt
@@ -1,9 +1,11 @@
 package com.example.suryarating
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_visits_page.*
+import kotlinx.android.synthetic.main.content_visits_page.*
 
 class VisitsPage : AppCompatActivity() {
 
@@ -14,6 +16,10 @@ class VisitsPage : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+        }
+        button_visits_next.setOnClickListener { _ ->
+            startActivity(Intent(this, ScrollingActivity::class.java))
+            // TODO replace this class with the one we actually want.
         }
     }
 }
