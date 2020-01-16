623af5e0f63ad18c4c56c7a87ada99f28e5e68de
aditi <ank55@cornell.edu>
2020-01-16 20:29:37 +0530

    added new pg

--- SuryaRating/app/src/main/java/com/example/suryarating/HearOfUsPage.kt
+++ SuryaRating/app/src/main/java/com/example/suryarating/HearOfUsPage.kt
@@ -1,25 +1,19 @@
 package com.example.suryarating
 
-import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_visits_page.*
-import kotlinx.android.synthetic.main.content_visits_page.*
+import kotlinx.android.synthetic.main.activity_hear_of_us_page.*
 
-class VisitsPage : AppCompatActivity() {
+class HearOfUsPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_visits_page)
+        setContentView(R.layout.activity_hear_of_us_page)
         setSupportActionBar(toolbar)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
-        }
-        button_visits_next.setOnClickListener { _ ->
-            startActivity(Intent(this, HearOfUsPage::class.java))
-            // TODO replace this class with the one we actually want.
         }
     }
 }
